4b6515f8fabf9d2fc769f4c88edb7937bcf00b2a
Sean Oliver <snoliver@umich.edu>
2018-10-20 15:50:29 -0400

    Added filecheck as seperated bash script Renamed to plural

---
+++ CodeBustersGrader.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash
+
+./filecheck $1
+# Run key_formatter.py
+./key_formatter.py KEY.txt
+# list files, pipe into grep and search for _NUM.txt file pattern
+# print those results into a variable
+studentAns=$(ls | grep -P '_\d+.txt')
+for file in $studentAns
+do
+	echo $file
+	
+done
--- filecheck.sh
+++ filecheck.sh
@@ -22,9 +22,10 @@ if [ ! -f key_formatter.py ]
 		echo "Get a correct copy of CodeBustersGrader at https://github.com/oliversno/CodeBustersGrader"
 		exit 1
 fi
-# Run key_formatter.py
-./key_formatter.py KEY.txt
-# list files, pipe into grep and search for _NUM.txt file pattern
-# print those results into a variable
-studentAns=$(ls | grep -P '_\d+.txt')
-echo $studentAns
+# Make sure student_answer_formatter.py exists
+if [ ! -f student_answer_formatter.py ]
+	then
+		echo "student_answer_formatter.py not found"
+		echo "Get a correct copy of CodeBustersGrader at https://github.com/oliversno/CodeBustersGrader"
+		exit 1
+fi
