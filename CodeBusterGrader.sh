b6e144932571998808b567536885658dbb6f0691
Sean Oliver <snoliver@umich.edu>
2018-10-20 02:04:54 -0400

    Check if KEY.txt file exists

--- CodeBusterGrader.sh
+++ CodeBusterGrader.sh
@@ -1,5 +1,17 @@
 #/usr/bin/bash
+
+# Make sure input is a .tar file
 if [[ $1 != *.tar ]]
+	then 
+		echo "Input must be a single .tar file"
+		exit 1
+fi
+# eXtract Ze Files
+tar -xzf $1
+
+if [ ! -f KEY.txt ]
 	then
-		echo "Input must be a single .tar file"	
-	fi
+		echo "No file name KEY.txt found."
+		echo "Either Answer Key does not exist or is not incorrectly"
+		exit 1
+fi
