4af57a0bee3fcf66fa4b7fb3f6ac8210b96d9f1a
Sean Oliver <snoliver@umich.edu>
2018-10-19 23:02:42 -0400

    Bug Fix Lines in python start at 0

--- key_formatter.py
+++ key_formatter.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python
 import sys
 from itertools import islice
 import re
+
 def fixLine(line):
+	print (line)
 	re.sub('\W+','', line)
 	line.upper()
+	print (line)
 	return
 	
 
 def main():
-	if (len.argv != 1):
-		print 'Incorrect Number of Arguments\n'
-		exit(1) 
-	input_name = sys.argv[0]
+	if (len(sys.argv) != 2):
+		print ('Incorrect Number of Arguments\n')
+		exit(1)
+	input_name = sys.argv[1]
 	with open(input_name) as fin:
-		for line in islice(fin, 2, None):
+		for line in islice(fin, 1, None):
 			fixLine(line)
+
+if __name__=='__main__':
+	main()
