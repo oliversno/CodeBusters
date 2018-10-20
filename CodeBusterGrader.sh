12b13d8dc65ed77a25a7fb1c0170c766a1a26552
Sean Oliver <snoliver@umich.edu>
2018-10-20 02:11:13 -0400

    Check if key_formatter.py exists

--- CodeBusterGrader.sh
+++ CodeBusterGrader.sh
@@ -8,10 +8,17 @@ if [[ $1 != *.tar ]]
 fi
 # eXtract Ze Files
 tar -xzf $1
-
+# Make sure KEY.txt was in the tarball
 if [ ! -f KEY.txt ]
 	then
-		echo "No file name KEY.txt found."
-		echo "Either Answer Key does not exist or is not incorrectly"
+		echo "No file name KEY.txt found"
+		echo "Either Answer Key does not exist or is  incorrectly named"
+		exit 1
+fi
+# Make sure key_formatter.py exists
+if [ ! -f key_formatter.py ]
+	then
+		echo "key_formatter.py not found"
+		echo "Please get a correct copy of CodeBustersGrader at https://github.com/oliversno/CodeBustersGrader"
 		exit 1
 fi
