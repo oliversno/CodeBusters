13228b436233267e728a478eed27d98be63d9b53
Sean Oliver <snoliver@umich.edu>
2018-12-26 16:38:01 -0500

    added support for numerical answers

--- grader.h
+++ grader.h
@@ -24,19 +24,38 @@ struct Score {
     std::vector<int> points;
 };
 
-void scoreAns(Question key, std::string ans, Score& team) {
-    int errors = -2;
-    for(int i = 0; i <key.ans.length(); i++) {
-        if(key.ans[i] != ans[i])
-            errors++;
+bool isNumber(std::string& str) {
+    std::string::const_iterator it = str.begin();
+    if(!isdigit(*it) && *it != '-'))
+        return false;
+    for(;it != str.end(); it++) {
+        if(!isdigit(*it))
+            return false;
     }
-    
-    if(errors <= 0) {
-        team.points.push_back(key.pts);
+    return true;
+}
+
+void scoreAns(Question key, std::string ans, Score& team) {
+    if(isNumber(key.ans)) {
+        int penalty = 0;
+        if(key.ans != ans)
+            penalty = key.pts
+        team.points.push_back(key.pts - penalty);
     }
     else {
-        int penalty = errors*100;
-        team.points.push_back(std::max(0, key.pts - penalty));
+        int errors = -2;
+        for(int i = 0; i <key.ans.length(); i++) {
+            if(key.ans[i] != ans[i])
+                errors++;
+        }
+
+        if(errors <= 0) {
+            team.points.push_back(key.pts);
+        }
+        else {
+            int penalty = errors*100;
+            team.points.push_back(std::max(0, key.pts - penalty));
+        }
     }
 }
 #endif // GRADER_H
