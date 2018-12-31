df3e0a13980114afb69296d7bf9a39c85b64e2a6
Sean Oliver <snoliver@umich.edu>
2018-12-31 18:34:04 -0500

    added grader.cpp and grader.h to bash driver branch

---
+++ grader.cpp
@@ -0,0 +1,61 @@
+#include "grader.h"
+
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    std::cerr << "Must have at least an answer key and one student answer sheet\n";
+    exit (1);
+  }
+    std::string arg = argv[1];
+    if (arg != "KEY.txt") {
+      std::cerr << "Answer Key not named correctly\n";
+      exit(1);
+    }
+    int numStudentAns = argc - 2;
+    std::ifstream fKeyIn;
+    fKeyIn.open(argv[1]);
+    std::vector<Score> teams;
+    std::vector<Question> answers;
+    std::stringstream header = nextLine(fKeyIn);
+    std::string header_str = header.str();
+    std::string tournament_name = header_str.substr(0, header_str.length() - 23);
+    while(!fKeyIn.eof()) {
+        std::stringstream line = nextLine(fKeyIn);
+        if(line.str() != "") {
+          Question correct;
+          line >> correct.ans >> correct.pts >> correct.tiebreaker_order;
+          answers.push_back(correct);
+        }
+    }
+    teams.resize(numStudentAns);
+    for(size_t i = 0; i < teams.capacity();  i++){
+        std::ifstream fFormIn;
+        fFormIn.open(argv[i+2]);
+        std::stringstream team_info = nextLine(fFormIn);
+        int num;
+        std::string name;
+        team_info >> num >> name;
+        teams[i].setTeamNum(num);
+        teams[i].setTeamName(name);
+        teams[i].setQuestions(answers);
+        nextLine(fKeyIn);
+        for(Question correct : answers) {
+          std::string team_ans;
+          fFormIn >> team_ans;
+          teams[i].scoreAns(correct, team_ans);
+        }
+      fFormIn.close();
+      teams[i].sumPoints();
+    }
+    // 22 is length of "Codebusters Answer Key" plus 1 for the tab
+    std::sort(teams.begin(), teams.end());
+    
+    std::ofstream fRankingsOut;
+    fRankingsOut.open(tournament_name + "_Codebusters_Rankings.txt");
+    fRankingsOut <<"Rank\tTeam Name\tTeam Number\tScore\n";
+    for(size_t i = 0; i < teams.size(); i++) {
+      fRankingsOut << std::to_string(i+1) << '\t' << teams[i].getTeamName() << '\t' <<
+        teams[i].getTeamNum() << '\t' << teams[i].getTotalScore() << '\n';
+    }
+    fRankingsOut.close();
+    fKeyIn.close();
+  }
---
+++ grader.h
@@ -0,0 +1,130 @@
+#ifndef GRADER_H
+#define GRADER_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+std::stringstream nextLine(std::istream& is) {
+    std::string temp;
+    std::getline(is, temp);
+    std::stringstream os(temp);
+    return os;
+}
+
+bool isNumber(std::string& str) {
+    std::string::const_iterator it = str.begin();
+    if(!isdigit(*it) && *it != '-')
+        return false;
+    for(;it != str.end(); it++) {
+        if(!isdigit(*it))
+            return false;
+    }
+    return true;
+}
+
+struct Question {
+    std::string ans;
+    int pts;
+    int tiebreaker_order;
+};
+
+class tieFunctor {
+    private:
+        int rank;
+    public:
+        tieFunctor(int in) {
+            rank = in;
+        }
+        bool operator()(Question q) {
+            return q.tiebreaker_order == rank;
+        }
+};
+
+class Score {
+    private:
+        std::string team_name;
+        int team_num;
+        std::vector<int> points;
+        int total_points;
+        std::vector<Question> questions;
+    public:
+    Score(void) { //ctor
+        team_name = "NONE";
+        team_num = -1;
+        total_points = -1;
+    }
+    ~Score(void) { //dtor
+        points.clear();
+    }
+    bool operator<(const Score& other_team) const {
+        if(total_points == other_team.total_points) {
+            size_t tie_number = 1;
+            while(tie_number <= questions.size()) {
+                tieFunctor pred(tie_number);
+                int pos = std::distance(questions.begin(), std::find_if(questions.begin(), questions.end(), pred));
+                if(points[pos] == other_team.points[pos]) {
+                    tie_number++;
+                }
+                else {
+                    return points[pos] > other_team.points[pos];
+                 }
+            }
+
+        }
+        return total_points > other_team.total_points;
+        //TODO: Logic for tiebreakers
+    }
+
+    void scoreAns(Question key, std::string ans) {
+        if(isNumber(key.ans)) {
+            int penalty = 0;
+            if(key.ans != ans)
+                penalty = key.pts;
+            points.push_back(key.pts - penalty);
+        }
+        else {
+            int errors = -2;
+            for(size_t i = 0; i < key.ans.length(); i++) {
+                if(key.ans[i] != ans[i])
+                    errors++;
+            }
+
+            if(errors <= 0) {
+                points.push_back(key.pts);
+            }
+            else {
+                int penalty = errors*100;
+                points.push_back(std::max(0, key.pts - penalty));
+            }
+        }
+    }
+    void sumPoints() {
+        total_points = std::accumulate(points.begin(), points.end(), 0);
+    }
+    std::string getTeamName() const {
+        return team_name;
+    }
+    int getTeamNum() const {
+        return team_num;
+    }
+    int getTotalScore() const {
+        return total_points;
+    }
+    void setTeamNum(const int num_in) {
+        team_num = num_in;
+    }
+    void setTeamName(std::string& name_in) {
+        team_name = name_in;
+    }
+    void setQuestions(std::vector<Question> q_in) {
+        questions = q_in;
+    }
+};
+
+
+#endif // GRADER_H
