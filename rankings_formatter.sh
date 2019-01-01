5c3489a79ad2b699e1c98e7996fe635f1fd1a3e7
Sean Oliver <snoliver@umich.edu>
2018-12-31 21:23:17 -0500

    formatted data in table

--- rankings_formatter.sh
+++ rankings_formatter.sh
@@ -13,7 +13,14 @@ echo $hrFormName
 rm -f "${hrFormName}"
 echo "Codebusters Rankings - ${tournamentName}" > "${hrFormName}"
 echo "***" >> "${hrFormName}"
-echo "/ Rank / Team Name / Team Number / Score /" >> "${hrFormName}"
-echo "/-----/:---------:/:-----------:/------:/" >> "${hrFormName}"
+echo "| Rank | Team Name | Team Number | Score |" >> "${hrFormName}"
+echo "|-----|:---------:|:-----------:|------:|" >> "${hrFormName}"
+rm -f temp.txt
+cp "$1" temp.txt
+sed -i '1 d' temp.txt
+sed -i 's/[[:space:]]\+/ | /g' temp.txt
+sed -i 's/^/| /' temp.txt
+sed -i 's/$/ |/' temp.txt
+cat temp.txt >> "${hrFormName}"
 cat "${hrFormName}"
-rm temp.txt
+rm -f temp.txt
