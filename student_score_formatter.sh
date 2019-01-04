eca20c2b77e4a0dd1c9842f0a8fb18b939441e17
Sean Oliver <snoliver@umich.edu>
2019-01-04 17:19:55 -0500

    read in KEY

--- student_score_formatter.sh
+++ student_score_formatter.sh
@@ -2,12 +2,21 @@
 
 echo $1 > temp.txt
 team=$(sed 's/.txt//' temp.txt)
-newFileName=$($team_$3_Score_Sheet.txt)
+newFileName="$team"_"$3"_Score_Sheet.txt
+echo $newFileName
 rm -f temp2.txt
 cp "$1" temp.txt
 sed -i '1 d' temp.txt
 cat temp.txt | tr '\r\n' ' ' > temp2.txt #replace \n with spaces
 studentAns=()
 read -a studentAns < temp2.txt
-echo ${studentAns[*]} 
+echo ${studentAns[*]}
+cp "$2" temp.txt
+rm -f temp2.txt temp3.txt
+sed -i '1 d' temp.txt
+cut -f1 -s temp.txt > temp2.txt
+cat temp2.txt | tr '\r\n' ' ' > temp3.txt #replace \n with spaces
+correctAns=()
+read -a correctAns < temp3.txt
+echo ${correctAns[*]}
 #echo "Student Answer	Correct Answer	Points	Diff" > "${newFileName}"
